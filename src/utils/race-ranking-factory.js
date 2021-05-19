ee0b66fe57c6bbc453983fe54a16ff1b4cba51f5
Phú Phan Văn <phudev95@gmail.com>
2021-05-19 13:23:06 +0700

    Initial commit

---
+++ src/utils/race-ranking-factory.js
@@ -0,0 +1,31 @@
+import { SCENARIO } from './constant';
+import { fakeUsers } from './fake-users';
+
+/**
+ * raceRankingFactory()
+ * @returns array of user
+ */
+export function raceRankingFactory(totalUsers) {
+  const users = JSON.parse(JSON.stringify(fakeUsers)).slice(0, totalUsers);
+
+  return (scenario = SCENARIO.SCENARIO_1) => {
+    if (scenario === SCENARIO.SCENARIO_1) {
+      const randomUserIndex = Math.floor(Math.random() * 2);
+      const temporaryValue = { ...users[randomUserIndex] };
+      temporaryValue.progress += Math.random() * 20;
+
+      users[randomUserIndex] = temporaryValue;
+    } else {
+      const randomUserIndex = Math.floor(Math.random() * totalUsers);
+
+      const temporaryValue = { ...users[randomUserIndex] };
+      temporaryValue.progress += Math.random() * 20;
+      users[randomUserIndex] = temporaryValue;
+    }
+
+    // Sort
+    users.sort((a, b) => b.progress - a.progress);
+
+    return users;
+  };
+}
